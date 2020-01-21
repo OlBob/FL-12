c8d77c3440feb6459cb8c64aafdda84527a12d88
Oleksandr <cerebrum94@gmail.com>
2020-01-21 18:59:59 +0200

    Modified Fighter class

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,94 +1,78 @@
 class Fighter {
 	constructor(obj) {
-			this._name = obj.name;
-			this._damage = obj.damage;
-			this._hp = obj.hp;
-			this._strength = obj.strength;
-			this._agility = obj.agility;
-			this._win = 0;
-			this._loss = 0;
-	}
+		this._name = obj.name;
+		this._damage = obj.damage;
+		this._hp = obj.hp;
+		this._strength = obj.strength;
+		this._agility = obj.agility;
+		this._win = 0;
+		this._loss = 0;
 
-	getName() {
-			return this._name;
-	}
-	getDamage() {
-			return this._damage;
-	}
-	getStrength() {
-			return this._strength;
-	}
-	getAgility() {
-			return this._agility;
-	}
-	getHealth() {
-			return this._hp;
-	}
+		return {
+			getName: () => this._name,
+			getDamage: () => this._damage,
+			getStrength: () => this._strength,
+			getAgility: () => this._agility,
+			getHealth: () => this._hp,
 
-	attack(secondFighter) {
-		let rndArg = 101,
-				random = Math.floor(Math.random() * rndArg),
-				probability = secondFighter.getStrength() + secondFighter.getAgility();
-			if (random >= probability) {
+			attack: (secondFighter) => {
+				const PROBABILITY = secondFighter.getStrength() + secondFighter.getAgility(),
+							VARIABLE = 101;
+				if (Math.floor(Math.random() * VARIABLE) >= PROBABILITY) {
 					console.log(`${this._name} makes ${this._damage} damage to ${secondFighter.getName()}`)
 					secondFighter.dealDamage(this._damage);
-			} else {
+				} else {
 					console.log(`${this._name} attack missed`);
-			}
-	}
-
-	logCombatHistory() {
-			let combatHistory = `Name: ${this._name}, Wins: ${this._win}, Losses: ${this._loss}`;
-			console.log(combatHistory);
-	}
-	heal(value) {
-			this._hp += value;
-			return this._hp;
-	}
-	dealDamage(value) {
-			this._hp -= value;
-			this._hp < 0 ? this._hp = 0 : this._hp;
-			return;
-	}
-	addWin() {
-			return this._win++;
-	}
-	addLoss() {
-			return this._loss++;
+				}
+			},
+			logCombatHistory: () => {
+				console.log(`Name: ${this._name}, Wins: ${this._win}, Losses: ${this._loss}`);
+			},
+			heal: (value) => {
+				this._hp += value;
+				return;
+			},
+			dealDamage: (value) => {
+				this._hp -= value;
+				this._hp < 0 ? this._hp = 0 : this._hp;
+				return;
+			},
+			addWin: () => this._win++,
+			addLoss: () => this._loss++
+		}
 	}
 }
 
 const battle = (fighter1, fighter2) => {
 	let cheked = false;
 	const check = () => {
-			cheked = true;
-			return !!(fighter1.getHealth() === 0 || fighter2.getHealth() === 0);
+		cheked = true;
+		return !!(fighter1.getHealth() === 0 || fighter2.getHealth() === 0);
 	};
-	if (check() || cheked === false) {
-			console.log('The Battle can not begin! One of the Fighters is dead.');
-			return;
+	if (check() || !cheked) {
+		let deadFighter = fighter1.getHealth() === 0 ? fighter1.getName() : fighter2.getName();
+		console.log(`The Battle can not begin! ${deadFighter} is dead.`);
+		return;
 	}
 
 	const fight = () => {
-			if (fighter1.getHealth() === 0) {
-					console.log(`${fighter2.getName()} has won`);
-					fighter1.addLoss();
-					fighter2.addWin();
-					return;
-			} else if (fighter2.getHealth() === 0) {
-					console.log(`${fighter1.getName()} has won`);
-					fighter2.addLoss();
-					fighter1.addWin();
-					return;
-			} else {
-					fighter1.attack(fighter2);
-					fighter2.attack(fighter1);
-
-					return fight();
-			}
+		if (fighter1.getHealth() === 0) {
+			console.log(`${fighter2.getName()} has won`);
+			fighter1.addLoss();
+			fighter2.addWin();
+			return;
+		} else if (fighter2.getHealth() === 0) {
+			console.log(`${fighter1.getName()} has won`);
+			fighter2.addLoss();
+			fighter1.addWin();
+			return;
+		} else {
+			fighter1.attack(fighter2);
+			fighter2.attack(fighter1);
+			return fight();
+		}
 	}
 	return fight();
-
 }
 
 
